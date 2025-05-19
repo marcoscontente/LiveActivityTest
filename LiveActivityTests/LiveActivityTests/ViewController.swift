216aa7189d7170328ba5b2ec7334176ab467751c
Marcos Contente <marcos.contente@hotmart.com>
2025-05-19 15:54:34 -0300

    Make project

---
+++ LiveActivityTests/LiveActivityTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveActivityTests
+//
+//  Created by Marcos Contente on 19/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
