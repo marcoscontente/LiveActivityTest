2ebec5a958a019e216dd66bf3c0a5ad292c594eb
Marcos Contente <marcos.contente@hotmart.com>
2025-05-19 15:58:55 -0300

    Make LiveActivityExtension

---
+++ LiveActivityTests/LiveActivityExtension/LiveActivityExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LiveActivityExtensionBundle.swift
+//  LiveActivityExtension
+//
+//  Created by Marcos Contente on 19/05/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiveActivityExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        LiveActivityExtension()
+    }
+}
