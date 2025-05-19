5598a4ee5368a8c4de36b44cbfbd1a1245241829
Marcos Contente <marcos.contente@hotmart.com>
2025-05-19 16:01:23 -0300

    Add ActivityModel

---
+++ LiveActivityTests/LiveActivityTests/ActivityModel.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityModel.swift
+//  LiveActivityTests
+//
+//  Created by Marcos Contente on 19/05/25.
+//
+
+import ActivityKit
+
+struct ActivityModel: ActivityAttributes {
+    struct ContentState: Codable, Hashable {
+
+    }
+
+    var text: String
+    var image: String
+    var timeLeft: Double
+}
--- LiveActivityTests/LiveActivityTests/ViewController.swift
+++ LiveActivityTests/LiveActivityTests/ViewController.swift
@@ -11,9 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let startLiveButton = UIButton(type: .system)
+        startLiveButton.setTitle("Star Live Activity", for: .normal)
+        startLiveButton.backgroundColor = .systemBlue
+        startLiveButton.setTitleColor(.white, for: .normal)
+        startLiveButton.layer.cornerRadius = 8
+        startLiveButton.translatesAutoresizingMaskIntoConstraints = false
+        startLiveButton.addTarget(self, action: #selector(startLiveActivity), for: .touchUpInside)
+
+        view.addSubview(startLiveButton)
+
+        NSLayoutConstraint.activate([
+            startLiveButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startLiveButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            startLiveButton.widthAnchor.constraint(equalToConstant: 200),
+            startLiveButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
     }
 
+    @objc func startLiveActivity() {
 
+    }
+
+    func initLiveActivity() {
+        let initialData = ActivityModel.ContentState()
+        let liveActivityData = ActivityModel(text: "Hello", image: "gear", timeLeft: 1000)
+
+        do {
+
+        } catch {
+
+        }
+
+    }
 }
-
